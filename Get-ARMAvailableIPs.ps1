319e523f9dfde3c038ffce6beba07843fdaab35c
Henry Robalino <animetauren@users.noreply.github.com>
2018-04-25 17:51:28 -0400

    Added initial support for HTML reports

--- Get-ARMAvailableIPs.ps1
+++ Get-ARMAvailableIPs.ps1
@@ -97,7 +97,7 @@ function New-IPv4toBin ($ipv4){
  return $binNum -join ""
 }
 
-#Global Variables
+#region Global Variables
 
 Write-Host "Checking if AzureRM Module is installed..."
 if((Get-Module -ListAvailable -Name AzureRM.*)) {
@@ -129,6 +129,20 @@ $table = @()
 $ipTable = @()
 $availTable = @()
 $nameTable= @()
+
+$css = @"
+<style>
+h1, h5, th { text-align: center; font-family: Segoe UI; }
+table { margin: auto; font-family: Segoe UI; box-shadow: 10px 10px 5px #888; border: thin ridge grey; }
+th { background: #0046c3; color: #fff; max-width: 400px; padding: 5px 10px; }
+td { font-size: 11px; padding: 5px 20px; color: #000; }
+tr { background: #b8d1f3; }
+tr:nth-child(even) { background: #dae5f4; }
+tr:nth-child(odd) { background: #b8d1f3; }
+</style>
+"@
+
+#endregion
 
 switch($Scope)
 {
@@ -329,6 +343,8 @@ $tableCountIndex--
 
 $file = "$Path\AvailableIps-for-Vnet-$vnetname-Subnet-$subnet.csv"
 $table | Select-Object IPAddress,Availability,ResourceName | Export-Csv -Path $file -NoTypeInformation | Out-Null
+
+#Import-CSV $file | ConvertTo-Html -Head $css -Body "<h1>Email Report</h1>`n<h5>Generated on $(Get-Date)</h5>" | Out-File "C:\temp\test.html"
 
 #
 $table = @()
@@ -558,6 +574,8 @@ $tableCountIndex--
 
 $file = "$Path\AvailableIps-for-Vnet-$SourceVNET-Subnet-$SourceSUB.csv"
 $table | Select-Object IPAddress,Availability,ResourceName | Export-Csv -Path $file -NoTypeInformation | Out-Null
+
+Import-CSV $file | ConvertTo-Html -Head $css -Body "<h1>Source VNET: $SourceVNET - Source Subnet: $SourceSUB</h1>" | Out-File "C:\temp\test.html"
 
 } #closes First If Loop checking Sub & VNET info.
 
